2892ac06cafbff732ded3791f2ad6bf9e110b33d
inotdopey <7011928+inotdopey@users.noreply.github.com>
2017-12-13 07:41:36 -0600

    Add files via upload

--- christmas.ino
+++ christmas.ino
@@ -15,7 +15,7 @@ FASTLED_USING_NAMESPACE
 //#define CLK_PIN   4
 #define LED_TYPE    WS2811
 #define COLOR_ORDER BRG
-#define NUM_LEDS    500
+#define NUM_LEDS    100
 CRGB leds[NUM_LEDS];
 
 #define BRIGHTNESS          255
@@ -40,20 +40,25 @@ void setup() {
 
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])( uint8_t);
-SimplePatternList gPatterns = { RedEater, redWithGlitter, solidRedGreen, SolidRedWhite, 
-              RedAndGreenBars, RedAndWhiteBars, GreenAndWhiteBars, GreenEater, greenWithGlitter}; //blendwave, warmWhiteShimmer, confetti, sinelon, juggle};
+SimplePatternList gPatterns = { RedEater, redFading, redWithGlitter, randomFades, solidRedGreen, SolidRedWhite, randomFades,
+              RedAndGreenBars, RedAndWhiteBars, GreenAndWhiteBars, randomFades, GreenEater, greenWithGlitter,
+              randomFades}; //blendwave, warmWhiteShimmer, confetti, sinelon, juggle};
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+boolean moveOn = false;
   
 void loop()
 {
+  //Check for manual next pattern
+  if( moveOn) { nextPattern(); }
   // Create Loop Index for some functions
   static uint8_t startIndex = 0;
   startIndex = startIndex + 1; /* motion speed */
     
   // Call the current pattern function once, updating the 'leds' array
   gPatterns[gCurrentPatternNumber]( startIndex);
+  
 
   // send the 'leds' array out to the actual LED strip
   FastLED.show();  
@@ -71,40 +76,47 @@ void nextPattern()
 {
   // add one to the current pattern number, and wrap around at the end
   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+  moveOn=false;
 }
 
 void redWithGlitter( uint8_t colorIndex) 
 {
   fill_solid( leds, NUM_LEDS, CRGB(255,0,0));
-  addGlitter(80);
+  addGlitter(80,5,CRGB::White,0);
+}
+
+void redFading( uint8_t colorIndex)
+{
+  fill_solid( leds, NUM_LEDS, CRGB(255,0,0));
+  uint8_t redBrightness = random8(50,200);
+  FastLED.setBrightness(redBrightness);
+  delay(300);
 }
 
 
 void greenWithGlitter( uint8_t colorIndex) 
 {
   fill_solid( leds, NUM_LEDS, CRGB(0,255,0));
-  addGlitter(80);
+  addGlitter(80,5,CRGB::White,50);
 }
 
-void addGlitter( fract8 chanceOfGlitter) 
+void addGlitter( fract8 chanceOfGlitter, int glitterCount, CRGB c1,int speeds) 
 {
   if( random8() < chanceOfGlitter) {
-    leds[ random16(NUM_LEDS) ] += CRGB::White; //CRGB::White;
-    leds[ random16(NUM_LEDS) ] += CRGB::White; //CRGB::White;
-    leds[ random16(NUM_LEDS) ] += CRGB::White; //CRGB::White;
-    leds[ random16(NUM_LEDS) ] += CRGB::White; //CRGB::White;
-    leds[ random16(NUM_LEDS) ] += CRGB::White; //CRGB::White;
-    //delay(1000);
+    for(int i=0; i<glitterCount; i++) {
+      leds[ random16(NUM_LEDS) ] += c1; //CRGB::White;
+    }
+    delay(speeds);
   }
 }
 
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
+void FillLEDsFromPaletteColors( uint8_t colorIndex, uint8_t maxBright, uint8_t minBright)
 {
-    uint8_t brightness = 255;
-
-    for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 3;
+    for( int j = maxBright; j >= minBright; j=j-5) {
+      for( int i = 0; i < NUM_LEDS; i++) {
+          leds[i] = ColorFromPalette( currentPalette, colorIndex, j, currentBlending);
+          colorIndex += 3;
+      }
     }
 }
 
@@ -112,7 +124,7 @@ void RedAndGreenBars( uint8_t colorIndex)
 {
   fadeToBlackBy( leds, NUM_LEDS, 10);
   SetupRedAndGreenPalette();              currentBlending = LINEARBLEND;
-  FillLEDsFromPaletteColors( colorIndex);
+  FillLEDsFromPaletteColors( colorIndex,255,255);
 }
 
 void SetupRedAndGreenPalette()
@@ -132,7 +144,7 @@ void RedAndWhiteBars( uint8_t colorIndex)
 {
   fadeToBlackBy( leds, NUM_LEDS, 10);
   SetupRedAndWhitePalette();              currentBlending = LINEARBLEND;
-  FillLEDsFromPaletteColors( colorIndex);
+  FillLEDsFromPaletteColors( colorIndex,255,255);
 }
 
 void SetupRedAndWhitePalette()
@@ -152,7 +164,7 @@ void GreenAndWhiteBars( uint8_t colorIndex)
 {
   fadeToBlackBy( leds, NUM_LEDS, 10);
   SetupGreenAndWhitePalette();              currentBlending = LINEARBLEND;
-  FillLEDsFromPaletteColors( colorIndex);
+  FillLEDsFromPaletteColors( colorIndex,255,255);
 }
 
 void SetupGreenAndWhitePalette()
@@ -171,21 +183,27 @@ void SetupGreenAndWhitePalette()
 void GreenEater( uint8_t colorIndex)
 {
   //fadeToBlackBy( leds, NUM_LEDS, 10);
-  colorRun( CRGB::Green, CRGB::Red, 1, 50);
+  colorRun( CRGB::Green, CRGB::Red, 1, 50, true);
+  moveOn = true;
 }
 
 void RedEater( uint8_t colorIndex)
 {
   //fadeToBlackBy( leds, NUM_LEDS, 10);
-  colorRun( CRGB::Red, CRGB::Green, 1, 50);
+  colorRun( CRGB::Red, CRGB::Green, 1, 50, false);
+  moveOn = true;
 }
 
-void colorRun(CRGB c, CRGB d, int width, int speed){
-  // Slide the leds in one direction
+void colorRun(CRGB c, CRGB d, int width, int speed, boolean bidirectional){
+  // Slide the leds in one direction or 2 base on boolean value bidirectional
   for(int i = 0; i <= NUM_LEDS-width; i++) {
     for(int j=0; j<width; j++){
       leds[i+j-1] = c;
       leds[i+j] = c;
+      if( bidirectional) {
+        leds[NUM_LEDS-i-j-1] = c;
+        leds[NUM_LEDS-i-j] = c;
+      }
     }
 
     delay(speed);
@@ -210,13 +228,13 @@ void solidRedGreen( uint8_t colorIndex)
 {
   fadeToBlackBy( leds, NUM_LEDS, 10);
   //AlternateTwoColor(CRGB::Red,CRGB::Green); 
-  AlternateTwoColorExpandable(CRGB::Red,CRGB::Green); 
+  AlternateTwoColorExpandable(CRGB::Red,CRGB::Green,255,0); 
 }
 
 void SolidRedWhite( uint8_t colorIndex)
 {
   fadeToBlackBy( leds, NUM_LEDS, 10);
-  AlternateTwoColorExpandable(CRGB::White,CRGB::Red); 
+  AlternateTwoColorExpandable(CRGB::White,CRGB::Red,255,255); 
 }
 
 void AlternateTwoColor(CRGB c1, CRGB c2){
@@ -230,29 +248,41 @@ void AlternateTwoColor(CRGB c1, CRGB c2){
   FastLED.show();
 }
 
-void AlternateTwoColorExpandable(CRGB c1, CRGB c2){
-  for(int i=0; i<NUM_LEDS-5; i++){
-    leds[i] = c1;
-    i++;
-    leds[i] = c1;
-    i++;
-    leds[i] = c1;
-    i++;
-    leds[i] = c2;
-    i++;
-    leds[i] = c2;
-    i++;
-    leds[i] = c2;
+void AlternateTwoColorExpandable(CRGB c1, CRGB c2, uint8_t maxBright, uint8_t minBright){
+  for( int j = maxBright; j >= minBright; j=j-1) {
+    FastLED.setBrightness(j);
+    for(int i=0; i<NUM_LEDS; i++){
+      leds[i] = c1;
+      i++;
+      if(i<NUM_LEDS) { leds[i] = c1; }
+      i++;
+      if(i<NUM_LEDS) { leds[i] = c1; }
+      i++;
+      if(i<NUM_LEDS) { leds[i] = c2; }
+      i++;
+      if(i<NUM_LEDS) { leds[i] = c2; }
+      i++;
+      if(i<NUM_LEDS) { leds[i] = c2; }
+    }
+    FastLED.show();
+    delay(50);
   }
-  FastLED.show();
 }
 
-void confetti() 
+void confetti(boolean randomColors, CRGB setColor, int speeds) 
 {
-  // random colored speckles that blink in and fade smoothly
   fadeToBlackBy( leds, NUM_LEDS, 10);
   int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+  if( randomColors) {
+    // random colored speckles that blink in and fade smoothly
+    leds[pos] += CHSV( gHue + random8(64), 200, 255); 
+  } else {
+    // random white speckles that blink in and fade smoothly
+    uint8_t redBrightness = random8(100,255);
+    leds[pos] = setColor;
+    FastLED.setBrightness(redBrightness);
+  }
+  delay(speeds);
 }
 
 void sinelon()
@@ -273,49 +303,26 @@ void juggle() {
   }
 }
 
-void warmWhiteShimmer(unsigned char dimOnly)
-{
-  uint8_t randomBulb =random8(NUM_LEDS);
-  Serial.begin(115200);
-  Serial.println(randomBulb);
-  for (int i = 0; i < 5; i += 2)
-  {
-    Serial.println(i);
-    Serial.println(randomBulb);
-    if (i = randomBulb) {
-      uint8_t redBrightness = 100;
-      uint8_t greenBrightness = 80;
-      uint8_t blueBrightness = 12;
-      
-      // warm white: red = x, green = 0.8x, blue = 0.125x
-      redBrightness = random8();
-      greenBrightness = redBrightness*4/5;
-      blueBrightness = redBrightness*8/10;
-      leds[i] += CHSV( redBrightness, greenBrightness, blueBrightness);
-      
-      // every odd LED gets set to a quarter the brighness of the preceding even LED
-      if (i + 1 < NUM_LEDS)
-      {
-        leds[i] += CHSV( redBrightness/4, greenBrightness/4, blueBrightness/4);
-      }
-    }
-  }
+void randomFades( uint8_t colorIndex) {
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int fadeType = random8(1,5);
+  Serial.println(fadeType);
+    Serial.println(fadeType);
+    switch (fadeType) {
+      case 1:
+        confetti(false,CRGB::Red,100);
+        //break;
+      case 2:
+        confetti(false,CRGB::Red,100); 
+        //break;
+      case 3:
+        confetti(false,CRGB::Green,100);
+        //break;
+      case 4:
+        confetti(false,CRGB::Green,100);
+        //break;
+      default:
+        confetti(false,CRGB::White,100);
+        //break;
+    } 
 }
-
-void blendwave() {
-  CRGB clr1;
-  CRGB clr2;
-  uint8_t loc1;
-  uint8_t loc2;
-  uint8_t speed;
-  speed = beatsin8(6,0,255);
-
-  clr1 = blend(CHSV(beatsin8(3,0,255),255,255), CHSV(beatsin8(4,0,255),255,255), speed);
-  clr2 = blend(CHSV(beatsin8(4,0,255),255,255), CHSV(beatsin8(3,0,255),255,255), speed);
-
-  loc1 = beatsin8(10,0,NUM_LEDS-1);
-  
-  fill_gradient_RGB(leds, 0, clr2, loc1, clr1);
-  fill_gradient_RGB(leds, loc1, clr2, NUM_LEDS-1, clr1);
-
-} // blendwave()
